7365f1ff62276b1930fa080626f322db9cfd4c5b
cColds <103373668+cColds@users.noreply.github.com>
2023-06-10 00:34:09 -0500

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <header>
+        <h1 className="header-text">puyo puyo tetramemo</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
