ede18853db734576a4ab478a437a718874b13b61
cColds <103373668+cColds@users.noreply.github.com>
2023-06-11 06:29:04 -0500

    Shuffle initial card state

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import Cards from "./Cards";
+import data from "../data/characters.json";
 
 export default function Game() {
+  function getShuffledCards(targetCards) {
+    const copy = [...targetCards];
+    for (let i = copy.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [copy[i], copy[j]] = [copy[j], copy[i]];
+    }
+
+    return copy;
+  }
+
+  const { characters } = data;
+  const [cards, setCards] = useState(getShuffledCards(characters));
+
   return (
     <div>
       <Cards />
     </div>
   );
 }
-// randomize cards function
-// style cards to see
-// add event listeners to the cards
