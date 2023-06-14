da69d6b666fb5a51a9749f73cfa31de2b7caee69
cColds <103373668+cColds@users.noreply.github.com>
2023-06-14 17:47:49 -0500

    Add more cards and update naming scheme

--- src/data/cards.js
+++ src/data/cards.js
@@ -2,29 +2,315 @@ import { v4 as uuidv4 } from "uuid";
 
 const cardsData = [
   {
-    name: "Ringo",
+    name: "ringo",
     image: "ringo.png",
     hasClicked: false,
     isCardInUse: false,
     id: uuidv4(),
   },
   {
-    name: "Arle",
+    name: "arle",
     image: "arle.png",
     hasClicked: false,
     isCardInUse: false,
     id: uuidv4(),
   },
   {
-    name: "Amitie",
+    name: "amitie",
     image: "amitie.png",
     hasClicked: false,
     isCardInUse: false,
     id: uuidv4(),
   },
   {
-    name: "Maguro",
+    name: "maguro",
     image: "maguro.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+  {
+    name: "carbuncle",
+    image: "carbuncle.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+  {
+    name: "sig",
+    image: "sig.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "risukuma",
+    image: "risukuma.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "suketoudara",
+    image: "suketoudara.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "witch",
+    image: "witch.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+  {
+    name: "draco centauros",
+    image: "draco-centauros.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "schezo",
+    image: "schezo.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "marle",
+    image: "marle.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "marle alt",
+    image: "marle-alt.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "ecolo",
+    image: "ecolo.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "ally",
+    image: "ally.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "tee",
+    image: "tee.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "ess",
+    image: "ess.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "ai",
+    image: "ai.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "jay & elle",
+    image: "jay-and-elle.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "zed",
+    image: "zed.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "klug",
+    image: "klug.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "lemres",
+    image: "lemres.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "raffina",
+    image: "raffina.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "rulue",
+    image: "rulue.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "feli",
+    image: "feli.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "dark prince",
+    image: "dark-prince.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "ex",
+    image: "ex.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "squares",
+    image: "squares.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "sonic",
+    image: "sonic.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "lidelle",
+    image: "lidelle.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "miss accord",
+    image: "miss-accord.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "ocean prince",
+    image: "ocean-prince.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "serilly",
+    image: "serilly.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+
+  {
+    name: "yu",
+    image: "yu.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+  {
+    name: "rei",
+    image: "rei.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+  {
+    name: "rafisol",
+    image: "rafisol.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+  {
+    name: "possessed klug",
+    image: "possessed-klug.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+  {
+    name: "harpy",
+    image: "harpy.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+  {
+    name: "legamunt",
+    image: "legamunt.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+  {
+    name: "ragnus the brave",
+    image: "ragnus-the-brave.png",
+    hasClicked: false,
+    isCardInUse: false,
+    id: uuidv4(),
+  },
+  {
+    name: "rozatte",
+    image: "rozatte.png",
     hasClicked: false,
     isCardInUse: false,
     id: uuidv4(),
